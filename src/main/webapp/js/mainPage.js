61cd0a7d937ec833d2a27d5529afd0eac022feb3
“王贵康 <15777239195@163.com>
2020-03-22 21:47:53 +0800

    提交登录和注册完整功能。登陆后，隐藏注册和登录按钮，显示退出按钮。

--- src/main/webapp/js/mainPage.js
+++ src/main/webapp/js/mainPage.js
@@ -20,12 +20,16 @@ $(function(){
 })
 
 
+/**
+ * 注册单击事件
+ * (实质上是请求后台插入一条数据)
+ */
 function regist(){
 	
 	//清除提示信息内容
-	$("#warning_1 span").html("");
-	$("#warning_2 span").html("");
-	$("#warning_3 span").html("");
+	$("#warning_1").html("");
+	$("#warning_2").html("");
+	$("#warning_3").html("");
 	
 	//获取输入框的值
 	var name=$("#mainPage_name").val().trim();
@@ -39,24 +43,28 @@ function regist(){
 	//检测用户名
 	if(name==""){
 		ok=false;
-		$("#warning_1 span").html("用户名为空");
-		$("#warning_1").show();
+		$("#warning_1").html("用户名为空");
+		//运行结束
+		return;
 	}
 	//检测密码
 	if(password==""){
 		ok=false;
-		$("#warning_2 span").html("密码为空");
-		$("#warning_2").show();
+		$("#warning_2").html("密码为空");
+		//运行结束
+		return;
 	}else if(password.length < 6){
 		ok=false;
-		$("#warning_2 span").html("密码长度小于6位");
-		$("#warning_2").show();
+		$("#warning_2").html("密码长度小于6位");
+		//运行结束
+		return;
 	}
 	//检测确认密码
 	if(final_password!=password){
 		ok=false;
-		$("#warning_3 span").html("输入密码不一致");
-		$("#warning_3").show();
+		$("#warning_3").html("输入密码不一致");
+		//运行结束
+		return;
 	}
 	if(ok){
 		$.ajax({
@@ -70,21 +78,21 @@ function regist(){
 			dataType:"json",
 			
 			success:function(result){
-				debugger;
 				if(result.state==0){
 					//获取到用户的所有信息
 					var user=result.data;
-					//关闭弹窗
-					
-					//返回登录页面
-//					$("#back").click();
-					//设置用户名输入域数据
-//					$("#count").val(user.name);
+					alert("注册成功！");
+					//关闭注册弹窗
+					cancelRegist();
+					//打开登陆弹框
+					showLogin();
+					//账号填充
+					$("#mainPage_loginName").val(user.account);
 					//密码输入域获得焦点
-//					$("#password").focus();
+//					$("#mainPage_loginPwd").focus();
+					
 				}else if(result.state==2){
-					$("#warning_1 span").html(result.message);
-					$("#warning_1").show();
+					$("#warning_1").html(result.message);
 				}
 			},
 			error:function(data){
@@ -97,22 +105,21 @@ function regist(){
 
 
 /**
- * 登陆
+ * 登录单击事件
  */
-function loginAction(){
+function login(){
 	debugger;
 	//alert("绑定成功!");
 	//清空提示信息
-	$("#count_span").html("");
+	$("#name_span").html("");
 	$("#password_span").html("");
 	//获取参数
-	var name =$("#count").val().trim();
-	var password=$("#password").val().trim();
-	//alert(name+","+password);
+	var name =$("#mainPage_loginName").val().trim();
+	var password=$("#mainPage_loginPwd").val().trim();
 	//参数数据格式校验
 	var ok=true;
 	if(name==""){
-		$("#count_span").html("用户名为空");
+		$("#name_span").html("用户名为空");
 		ok=false;
 	}
 	if(password==""){
@@ -128,10 +135,18 @@ function loginAction(){
 			dataType:"json",
 			data:{"account":name,"pswd":password},
 			success:function(result){
-				if(result.state==0){//成功
+				if(result.state==0){
+					//成功
 					var user=result.data;
-					addCookie("userId",user.id,2);
-					window.location.href="activity.html";
+					//
+//					addCookie("userId",user.id,2);
+					//关闭弹窗
+					cancelLogin();
+					//隐藏注册和登录按钮，显示退出按钮
+					$("#mainPage_registWindown").attr("style","display:none;");
+					$("#mainPage_loginWindown").attr("style","display:none;");
+					$("#mainPage_signOut").attr("style","display:block;");
+					
 				}else if(result.state==2){//用户名错误
 					$("#count_span").html(result.message);
 				}else if(result.state==3){//密码错误
@@ -153,6 +168,11 @@ function loginAction(){
  */	
 function registWindown(){
 	$("#mainPage_registModal").show();
+	//清空输入框
+	$("#mainPage_name").val('');
+	$("#mainPage_nickname").val('');
+	$("#mainPage_password").val('');
+	$("#mainPage_conpassword").val('');
 }
 
 /**
@@ -167,6 +187,8 @@ function cancelRegist(){
  */	
 function showLogin(){
 	$("#mainPage_loginModal").show();
+	//密码输入框一定是清空的
+	$("#mainPage_loginPwd").val('');
 }
 
 /**
